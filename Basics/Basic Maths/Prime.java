f6caaf51e67cff8303b4e5efe662100cfe23e02c
Nikshep <nikshepd01@gmail.com>
2025-08-17 23:59:35 +0530

    program updated

---
+++ Basics/Basic Maths/Prime.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num = sc.nextInt();
+        boolean ans = check(num);
+        System.out.println(ans);
+    }
+    static boolean check(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
