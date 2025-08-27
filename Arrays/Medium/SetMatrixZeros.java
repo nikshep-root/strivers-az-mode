e53c830d90e26b8d9d90f5e410a8984e82f4303c
Nikshep <nikshepd01@gmail.com>
2025-08-27 23:58:06 +0530

    programs updated

--- Arrays/Medium/SetMatrixZeros.java
+++ Arrays/Medium/SetMatrixZeros.java
@@ -1,5 +1,29 @@
+import java.util.Arrays;
 public class SetMatrixZeros {
     public static void main(String[] args) {
-        int[] nums = {{1,1,1},{1.0,1},{1,1,1}};
+        int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
+        int[][] ans = setZeros(matrix);
+        System.out.println(Arrays.deepToString(ans));
+    }
+    static int[][] setZeros(int[][] matrix) {
+        int n = matrix.length;
+        int[] row = new int[n];
+        int[] column = new int [n];
+        for(int i = 0; i < n;i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    column[j] = 1;
+                }
+            }
+        }
+        for(int i = 0; i < n;i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                if(row[i] == 1|| column[j] == 1){
+                     matrix[i][j] = 0;
+                }           
+            }
+        }
+        return matrix;
     }
 }
