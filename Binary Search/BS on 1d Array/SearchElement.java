fbed532efb888c196c87995474f147a46b342326
Nikshep <nikshepd01@gmail.com>
2025-09-01 00:08:40 +0530

    corrected code

--- Binary Search/BS on 1d Array/SearchElement.java
+++ Binary Search/BS on 1d Array/SearchElement.java
@@ -9,16 +9,16 @@ public class SearchElement {
     static int search(int[] nums, int target){
         int start = 0;
         int end = nums.length - 1;
-        int mid = start + (end - start)/2;
         while(start <= end) {
+            int mid = start + (end - start)/2;
             if(nums[mid] == target){
                 return mid;
             }
             if(nums[mid] < target){
-                end--;
+                start = mid + 1;
             }
             else{
-                start++;
+                end = mid - 1;
             }
         }
         return -1;
